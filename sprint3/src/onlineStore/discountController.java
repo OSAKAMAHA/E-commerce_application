f2988e6204a0984e163ffa11cbae558dc2314164
OSAKAMAHA <osamaakhader@gmail.com>
2019-12-17 15:48:17 +0200

    added user story 3

---
+++ sprint3/src/onlineStore/discountController.java
@@ -0,0 +1,45 @@
+package onlineStore;
+
+import java.sql.SQLException;
+
+public class discountController {
+	private business current;
+	private product pro;
+	String output;
+	private boolean boughtBefore() throws ClassNotFoundException, SQLException {
+		dataBaseConnection db = new dataBaseConnection();
+		String q = "select count * from(select * from orders where orderedproductID = "+pro.getID()+"and ownerUserName = '"+current.getLoginInfo().getUsername()+"')";
+		db.rs = db.st.executeQuery(q);
+	    db.rs.next();
+	    if(db.rs.getInt(1) > 0) {
+	    	return true;
+	    }else {
+	    	return false;
+	    }
+	}
+	public String disc(business current,product pro,int quantity) {
+		this.current = current;
+		this.pro = pro;
+		double discAmount = 0;
+		output = "";
+		if(current.isStoreOwner()) {
+			output+="you got 15% because you are a store owner \n";
+			discAmount += 0.15;
+		}
+		if(quantity >= 2) {
+			output+="you got 10% because you bought 2 or more items of the same product \n";
+			discAmount += 0.10;
+		}
+		try {
+			if(!boughtBefore()) {
+				output+="you got 5% because it's the first time buying this product \n";
+				discAmount += 0.5;
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		pro.setPrice((int)(pro.getPrice()*(1-discAmount)));
+		return output;
+	}
+}
--- sprint3/src/onlineStore/orderController.java
+++ sprint3/src/onlineStore/orderController.java
@@ -6,11 +6,13 @@ public class orderController {
 	orderHandler oh;
 	business current;
 	ArrayList<product> prods;
+	discountController dc;
 	int choice;
 	orderController(business current){
 		this.current = current;
 		oh = new orderHandler();
 		prods = new ArrayList<product>();
+		dc = new discountController();
 	}
 	public String View() {
 		prods = oh.viewProducts();
@@ -27,19 +29,21 @@ public class orderController {
 	public String viewProd(int choice) {
 		this.choice = choice;
 		String output = "";
-		output+="product info \n";
-		oh.updateViews(prods.get(choice-1));
+		product curr = prods.get(choice-1);
+		output+=curr.getPp().getName()+"\n";
+		output+=curr.getPrice()+"\n";
+		oh.updateViews(curr);
 		return output;
 	}
 	public String buy(int quantity,String shippingAddress) {
-		Order ord = current.makeOrder(prods.get(choice-1), quantity, shippingAddress);
 		String output = "";
+		output+= dc.disc(current, prods.get(choice-1), quantity);
+		Order ord = current.makeOrder(prods.get(choice-1), quantity, shippingAddress);
 		if(oh.addOrder(ord)) {
-			output = "order made successfully \n";
+			output += "order made successfully \n";
 			oh.updateQuantity(ord);
 		}else {
 			output = "failed to make order";
 		}return output;
-		
 	}
 }
