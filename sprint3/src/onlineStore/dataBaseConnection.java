b64639bcd055c6e1928848390117c43ef400c71f
OSAKAMAHA <osamaakhader@gmail.com>
2019-12-18 00:22:54 +0200

    made the database online

--- sprint3/src/onlineStore/dataBaseConnection.java
+++ sprint3/src/onlineStore/dataBaseConnection.java
@@ -8,7 +8,7 @@ public class dataBaseConnection {
 	public ResultSet rs;
 	public dataBaseConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-		Connection con = DriverManager.getConnection("jdbc:sqlserver://localhost;databaseName=OnlineStore;integratedSecurity=true;");
+		Connection con = DriverManager.getConnection("jdbc:sqlserver://SQL5014.site4now.net;databaseName=DB_A50F4B_OnlineStore;user=DB_A50F4B_OnlineStore_admin;password=password123;");
 		st = con.createStatement();
 	}
 }
